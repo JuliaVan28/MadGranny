3ec38cd156ad2b7b5058e65e48189bee1b8d4479
Yuliia Vanchytska <julia.van2801@gmail.com>
2023-12-07 12:35:56 +0100

    Initial Commit

---
+++ MadGranny/MadGrannyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MadGrannyApp.swift
+//  MadGranny
+//
+//  Created by Yuliia on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MadGrannyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
